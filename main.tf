97efe400e5ef99e5612f53dd122cf937324aa9b5
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-03 14:07:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,3 +80,4 @@ resource "aws_api_gateway_method_response" "api_gateway_method_response" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_gateway_resource.id
   http_method   = aws_api
+}
