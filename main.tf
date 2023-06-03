87c72497c9c0c2d75ace1afc1463be02f0c8ad39
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-03 14:17:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -80,5 +80,8 @@ resource "aws_api_gateway_method_response" "api_gateway_method_response" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_gateway_resource.id
   http_method   = aws_api_gateway_method.api_gateway_method.http_method
-  status_code   = "200"  # Replace with the desired status code
+
+  response_models = {
+    "application/json" = "Empty"
+  }
 }
