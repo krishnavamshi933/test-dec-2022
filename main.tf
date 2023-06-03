52a0223dc0c958fe70174e0a0c05f19eeba2fcab
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-03 14:10:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -79,5 +79,6 @@ resource "aws_api_gateway_integration" "api_gateway_integration" {
 resource "aws_api_gateway_method_response" "api_gateway_method_response" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_gateway_resource.id
-  http_method   = aws_api
+  http_method   = aws_api_gateway_method.api_gateway_method.http_method
+  status_code   = "200"  # Replace with the desired status code
 }
