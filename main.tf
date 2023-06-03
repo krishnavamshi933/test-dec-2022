cc90b7bb318ce43f755311e5edf54d8b9a568341
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2023-06-03 15:44:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-# main.tf
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-west-2"  # Update with your desired region
+  region = "us-west-2"  # Replace with your desired region
 }
 
 resource "aws_lambda_function" "spring_app" {
   function_name = "my-spring-app"
   role          = aws_iam_role.lambda_exec_role.arn
-  handler       = "com.example.HelloWorldApplication::handleRequest"  # Update with your actual handler class and method
+  handler       = "com.example.MySpringApp::handleRequest"  # Replace with your actual handler class and method
 
   runtime = "java11"
   timeout = 10
@@ -91,23 +80,5 @@ resource "aws_api_gateway_method_response" "api_gateway_method_response" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
   resource_id   = aws_api_gateway_resource.api_gateway_resource.id
   http_method   = aws_api_gateway_method.api_gateway_method.http_method
-  status_code   = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "api_gateway_integration_response" {
-  rest_api_id     = aws_api_gateway_rest_api.api_gateway.id
-  resource_id     = aws_api_gateway_resource.api_gateway_resource.id
-  http_method     = aws_api_gateway_method.api_gateway_method.http_method
-  status_code     = aws_api_gateway_method_response.api_gateway_method_response.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-  }
+  status_code   = "200"  # Replace with the desired status code
 }
